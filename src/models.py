a77d85cd6460f6e81470665f6aa50a4238f5af55
Emilio Acosta <139580611+Emilioone@users.noreply.github.com>
2023-11-09 19:51:55 +0000

    tablas

--- src/models.py
+++ src/models.py
@@ -1,19 +1,85 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    email= db.Column(db.String(250), nullable=False, unique=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    favorite = db.relationship("Favorite", uselist=True, backref='user') 
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "created": self.created    # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    height = db.Column(db.String(120), nullable=False)
+    mass = db.Column(db.String(120), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    favorite = db.relationship('Favorite', backref='people')
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "created": self.created
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(120), nullable= False)
+    diameter = db.Column(db.String(120), nullable= False)
+    population = db.Column(db.String(120), nullable= False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "population": self.population,
+            "mass": self.mass,
+            "created": self.created
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    user_id = db.Column(db.Integer, db.ForeignKey ('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey ('people.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey ('planet.id'))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "created": self.created,
+            "planets": self.planets
+        }
+    
+
